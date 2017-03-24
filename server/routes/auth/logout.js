85232292c13b63370925ea14ddb64ee1814b703a
Dana Woodman <dana@danawoodman.com>
2017-03-24 11:22:15 -0700

    Redirect with logout

--- server/routes/auth/logout.js
+++ server/routes/auth/logout.js
@@ -2,5 +2,13 @@ module.exports = function logout(req, res) {
   req.currentUser = null
   req.session.currentUserId = null
   res.locals.currentUser = null
-  res.redirect('/')
+
+  let redirect = '/'
+  if (req.query.redirect && req.query.redirect !== '/auth/logout') {
+    redirect = req.query.redirect
+  }
+
+  console.log('[logout] Logging out and redirecting to:', redirect)
+
+  res.redirect(redirect)
 }
