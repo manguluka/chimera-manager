97e710741795460172bf49c3d6019d157fde16f9
Dana Woodman <dana@danawoodman.com>
2017-03-25 23:26:29 -0700

    Load css derp.

--- frontend/index.js
+++ frontend/index.js
@@ -1,7 +1,7 @@
 //require('isomorphic-fetch')
 //const React = require('react')
 //const ReactDOM = require('react-dom')
-//require('./styles/index.scss')
+require('./styles/index.scss')
 
 console.log('Frontend loaded with environment:', process.env.NODE_ENV)
 
