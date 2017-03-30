192b2b0bbe8b809828a1d35b426ab6b0101bd804
Dana Woodman <dana@danawoodman.com>
2017-03-30 16:49:48 -0700

    Fix up instructor activity logging.

--- server/routes/instructors/create.js
+++ server/routes/instructors/create.js
@@ -7,11 +7,17 @@ const wrap = require('../../lib/express-async-wrapper')
 module.exports = wrap(async function createInstructor(req, res, next) {
   const event = await Event.findOne(req.params.id)
   const userId = req.body.instructor
-  const instructor = await Instructor.create({
+  await Instructor.create({
     eventId: event.id,
     userId,
   })
-  await Activity.record({
+
+  req.flash('success', 'Instructor added')
+  res.redirect(`/events/${event.id}`)
+
+  // Do this last and don't block on it
+  const instructor = await User.findOne(userId)
+  Activity.record({
     action: 'added instructor',
     model: event,
     user: res.locals.currentUser,
@@ -19,7 +25,5 @@ module.exports = wrap(async function createInstructor(req, res, next) {
       instructor,
     },
   })
-  req.flash('success', 'Instructor added')
-  res.redirect(`/events/${event.id}`)
 })
 
--- server/routes/instructors/remove.js
+++ server/routes/instructors/remove.js
@@ -1,12 +1,14 @@
 const Activity = require('../../models/activity')
 const Event = require('../../models/event')
 const Instructor = require('../../models/instructor')
+const User = require('../../models/user')
 const wrap = require('../../lib/express-async-wrapper')
 
 module.exports = wrap(async function remove(req, res, next) {
   const eventId = Number(req.params.id)
   const userId = Number(req.params.userId)
   console.log('[destroyInstructor] Removing instructor:', { eventId, userId })
+
   await Instructor.destroy({
     where: {
       eventId: { equals: eventId },
@@ -18,10 +20,14 @@ module.exports = wrap(async function remove(req, res, next) {
 
   // Do this last and don't block on it
   const event = await Event.findOne(eventId)
+  const instructor = await User.findOne(userId)
   Activity.record({
     action: 'removed instructor',
     model: event,
     user: res.locals.currentUser,
+    extraInfo: {
+      instructor,
+    }
   })
 })
 
